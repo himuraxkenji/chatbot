6a343abad9d6f03e0bad03ab6ba563714890d388
himuraxkenji <aaron.ariperto@gmail.com>
2021-02-24 10:32:30 -0300

    feat: se creo chatbot

---
+++ nltk_utils.py
@@ -0,0 +1,28 @@
+import nltk
+nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+import numpy as np
+
+stemmer = PorterStemmer()
+
+def tokenized(sentence):
+    return nltk.word_tokenize(sentence)
+
+def steamming(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence, all_words):
+    """
+    sentence: ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thanks", "cool"]
+    bog = [0, 1, 0, 1, 0, 0, 0]
+    """
+    tokenized_sentence = [steamming(w) for w in tokenized_sentence]
+
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] =  1.0
+
+    return bag
+
